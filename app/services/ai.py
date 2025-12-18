1e5f06152535c78bf3e28b86653eeed18ccc917f
Softjson <softjson@gmail.com>
2025-12-18 18:39:08 -0500

    cambio en el modelo de ia en uso

--- app/services/ai.py
+++ app/services/ai.py
@@ -6,10 +6,8 @@ client = genai.Client(api_key=settings.gemini_api_key)
 
 def generate_post(prompt: str) -> str:
     try:
-        for model in client.models.list():
-            print(f"Modelo disponible: {model.name}")
         response = client.models.generate_content( 
-            model="gemini-2.0-flash-lite", contents=prompt)
+            model="gemini-2.5-flash-lite", contents=prompt)
         return response.text
     except ServerError:
         return "El modelo de IA está temporalmente ocupado. Intenta nuevamente."
